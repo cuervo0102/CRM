3e1365ac20ca59d98e5e80131bc8c75df2f88c99
Saikaisiham <blackbutlersiham2001@gmail.com>
2024-04-16 23:13:33 +0100

    commit

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import login_request
+from .views import login_request, logout_request
 
 
 
 urlpatterns = [
-    path('', login_request, name='login_request')
+    path('', login_request, name='login_request'), 
+    path('logout', logout_request, name='logout_request')
 ]
